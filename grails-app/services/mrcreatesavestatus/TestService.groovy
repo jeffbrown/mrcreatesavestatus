95ac4b7f2826b94746bc8825fc94dbaa6812ee66
Jeff Scott Brown <brownj@objectcomputing.com>
2020-12-11 07:21:05 -0600

    remove unused methods

--- grails-app/services/mrcreatesavestatus/TestService.groovy
+++ grails-app/services/mrcreatesavestatus/TestService.groovy
@@ -4,16 +4,7 @@ import grails.gorm.services.Service
 
 @Service(Test)
 interface TestService {
-
     Test get(Serializable id)
-
-    List<Test> list(Map args)
-
-    Long count()
-
-    void delete(Serializable id)
-
-    Test save(Test test)
 
     Test save(Boolean status)
 
